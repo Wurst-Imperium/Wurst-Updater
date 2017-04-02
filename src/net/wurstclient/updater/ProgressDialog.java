0a53fdc5c2db0b93fa6b898e2392f4519b10b935
Alexander01998 <contact.wurstimperium@gmail.com>
2017-04-02 10:36:00 +0200

    tk.wurst_client > net.wurstclient

--- src/net/wurstclient/updater/ProgressDialog.java
+++ src/net/wurstclient/updater/ProgressDialog.java
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package tk.wurst_client.updater;
+package net.wurstclient.updater;
 
 import javax.swing.JDialog;
 import java.awt.GridBagLayout;
